afa65d720548fc76b77e999baad4c3e54e3af52d
Scott Sullivan <scott@sullysbrain.com>
2023-10-17 22:38:36 -0400

    Added testing for Post object

--- src/pages/tests.py
+++ src/pages/tests.py
@@ -1,39 +1,23 @@
-from django.test import SimpleTestCase
+from django.test import TestCase
 from django.urls import reverse
 
-# Create your tests here.
-class HomepageTests(SimpleTestCase):
+from .models import Post
+
+class PostTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.post = Post.objects.create(body="This is a test!")
+
+    def test_model_content(self):
+        self.assertEqual(self.post.body, "This is a test!")
+
     def test_url_exists_at_correct_location(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
 
-    def test_url_available_by_name(self):
+    def test_homepage(self):
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
-
-    def test_template_name_correct(self):
-        response = self.client.get(reverse('home'))
-        self.assertTemplateUsed(response, 'home.html')
-
-    def test_template_content(self):
-        response = self.client.get(reverse('home'))
-        self.assertContains(response, '<h1>Homepage</h1>')
-
-
-class AboutpageTests(SimpleTestCase):
-    def test_url_exists_at_correct_location(self):
-        response = self.client.get("/about/")
-        self.assertEqual(response.status_code, 200)
-
-    def test_url_available_by_name(self):
-        response = self.client.get(reverse("about"))
-        self.assertEqual(response.status_code, 200)
-
-    def test_template_name_correct(self):
-        response = self.client.get(reverse('about'))
-        self.assertTemplateUsed(response, 'about.html')
-
-    def test_template_content(self):
-        response = self.client.get(reverse('about'))
-        self.assertContains(response, '<h1>About Page</h1>')
-
+        self.assertTemplateUsed(response, "home.html")
+        self.assertContains(response, "This is a test!")
