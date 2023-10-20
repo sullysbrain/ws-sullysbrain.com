adf95182758e94eb6d653aba65395cecc2fe9f31
Scott Sullivan <scott@sullysbrain.com>
2023-10-19 22:52:54 -0400

    New updates for fly deployment

--- src/sullysbrain/settings.py
+++ src/sullysbrain/settings.py
@@ -15,6 +15,9 @@ import os
 from pathlib import Path
 from environs import Env
 
+env = Env()
+env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -61,6 +64,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    "whitenoise.runserver_nostatic",  # new
     'django.contrib.staticfiles',
     'pages', # new pages
 ]
@@ -68,6 +72,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",  # new
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -101,9 +106,7 @@ WSGI_APPLICATION = 'sullysbrain.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        "default": env.dj_db_url("DATABASE_URL", default="sqlite:///db.sqlite3")
-    }
+    "default": env.dj_db_url("DATABASE_URL", default="sqlite:///db.sqlite3"),
 }
 #        'USER': 'username',
 #        'PASSWORD': get_secret('DB_PASSWORD'),
@@ -146,6 +149,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STORAGES = {
+    'default': {
+        'BACKEND': 'django.core.files.storage.FileSystemStorage',
+    },
+    'staticfiles': {
+        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+    },
+}
 
 
 # Default primary key field type
