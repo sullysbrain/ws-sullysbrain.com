011909f6c0dec7444eef19d51bbc65274c039d08
Scott Sullivan <scott@sullysbrain.com>
2023-10-24 18:55:11 -0400

    More troubleshooting for env transfer

--- src/sullysbrain/settings.py
+++ src/sullysbrain/settings.py
@@ -18,6 +18,7 @@ from environs import Env
 env = Env()  # new
 env.read_env()  # new
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
