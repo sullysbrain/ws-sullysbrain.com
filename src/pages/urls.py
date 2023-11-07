6c39f4ebaec0f8df068fa75d2ecf2c18a3ac23dc
Scott Sullivan <scott@sullysbrain.com>
2023-11-06 21:03:20 -0500

    Added Delete functionality to posts

--- src/pages/urls.py
+++ src/pages/urls.py
@@ -5,11 +5,13 @@ from .views import (
     BlogDetailView, 
     BlogCreateView,
     BlogUpdateview,
+    BlogDeleteView,
 )
 
 urlpatterns = [
     path("post/new/", BlogCreateView.as_view(), name="post_new"), 
     path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
     path("post/<int:pk>/edit/", BlogUpdateview.as_view(), name="post_edit"),
+    path("post/<int:pk>/delete/", BlogDeleteView.as_view(), name="post_delete"),
     path("", BlogListView.as_view(), name="home"),
 ]
